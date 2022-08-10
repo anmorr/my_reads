5e0eedaa33c1ae0511a97fb8e91e30102296ae8f
Anthony Morris <myThinkTank@anthonys-mbp.myfiosgateway.com>
2022-08-10 08:19:16 -0400

    Added boolean check for authors before join
    There was a bug where the author's information
    was being joined without first checking that the
    authors exists.
    
    Ref: Book.js lines 49-51

--- src/components/Book.js
+++ src/components/Book.js
@@ -1,69 +1,57 @@
-import * as BooksAPI from '../BooksAPI'
+import * as BooksAPI from "../BooksAPI";
 
-const Book = ( {
-    imageURL, 
-    title, 
-    authors,
-    bookshelf,
-    books,
-    setBooks,
-    id
-    } ) => {
+const Book = ({ imageURL, title, authors, bookshelf, books, setBooks, id }) => {
+  const addBook = async (book, shelf) => {
+    const res = await BooksAPI.update(book, shelf);
+  };
 
-        
-        const addBook = async (book, shelf) => {
-            const res = await BooksAPI.update(book, shelf)
-        }
-
-        
-
-
-        const selectionHandler = (selection) => {
-            const newBook = {
-                id:id,
-                title: title,
-                authors: authors,
-                imageURL: imageURL,
-                shelf: selection
-            }
-            addBook(newBook, selection)
-            
-            
-        }
-    return (
-        <li>
-            <div className="book">
-            <div className="book-top">
-                <div
-                className="book-cover"
-                style={{
-                    width: 128,
-                    height: 193,
-                    backgroundImage:
-                    `url(${imageURL})`,
-                }}
-                ></div>
-                <div className="book-shelf-changer">
-                <select 
-                onChange={(e) => selectionHandler(e.target.value)}
-                defaultValue={bookshelf ? bookshelf : "none"}
-                >
-                <option value="placeholder" disabled>Move to...</option>
-                {/* { !bookshelf === "currentlyReading" ? 
+  const selectionHandler = (selection) => {
+    const newBook = {
+      id: id,
+      title: title,
+      authors: authors,
+      imageURL: imageURL,
+      shelf: selection,
+    };
+    addBook(newBook, selection);
+  };
+  return (
+    <li>
+      <div className="book">
+        <div className="book-top">
+          <div
+            className="book-cover"
+            style={{
+              width: 128,
+              height: 193,
+              backgroundImage: `url(${imageURL})`,
+            }}
+          ></div>
+          <div className="book-shelf-changer">
+            <select
+              onChange={(e) => selectionHandler(e.target.value)}
+              defaultValue={bookshelf ? bookshelf : "none"}
+            >
+              <option value="placeholder" disabled>
+                Move to...
+              </option>
+              {/* { !bookshelf === "currentlyReading" ? 
                 <option value="currentlyReading" selected>Currently Reading</option> : */}
-                <option value="currentlyReading">Currently Reading</option>
-                {/* } */}
-                    <option value="wantToRead">Want to Read</option>
-                    <option value="read">Read</option>
-                    <option value="none" >None</option>
-                </select>
-                </div>
-            </div>
-            <div className="book-title">{title}</div>
-            <div className="book-authors">{authors.join('\r\n')}</div>
-            </div>
-        </li>
-    )
-}
+              <option value="currentlyReading">Currently Reading</option>
+              {/* } */}
+              <option value="wantToRead">Want to Read</option>
+              <option value="read">Read</option>
+              <option value="none">None</option>
+            </select>
+          </div>
+        </div>
+        <div className="book-title">{title}</div>
+        <div className="book-authors">
+          {authors ? authors.join("\r\n") : ""}
+        </div>
+      </div>
+    </li>
+  );
+};
 
 export default Book;
